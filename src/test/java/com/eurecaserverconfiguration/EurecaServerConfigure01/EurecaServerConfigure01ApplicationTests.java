38ea8cd188a53abd27eb3c1efa5a6a16f2429cc7
LAPTOP-3RLF7SP0\SIDD <siddharthkamble1992@gmail.com>
2021-07-09 19:58:28 +0530

    first commit

---
+++ src/test/java/com/eurecaserverconfiguration/EurecaServerConfigure01/EurecaServerConfigure01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurecaserverconfiguration.EurecaServerConfigure01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurecaServerConfigure01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
